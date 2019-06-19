db7faa27214b95ed3c6e4ab49f542ce6dd4354f2
Someguy300 <Someguy300@users.noreply.github.com>
2019-06-19 15:51:53 -0400

    Probando a ver como va esto

--- ProyectoJuego/src/proyectojuego/Juego.java
+++ ProyectoJuego/src/proyectojuego/Juego.java
@@ -14,8 +14,35 @@ public class Juego {
     char ent = 'E';
     char sal = 'S';
     
-    public void laberinto(){
-        
+    char [][]lab1 = new char[10][10];
+    
+    public char [][] setupLaberinto(char [][] vec){
+        for (int i = 0; i < vec.length; i++) { //Setup del laberinto
+            for (int j = 0; j < vec[i].length; j++) {
+                vec[i][j] = p;
+                
+            }  
+        }
+        return vec;
+    }
+    
+    
+    public void impresionLab(char [][] vec){
+        for (int i=0; i < vec.length; i++) { //Impresion del laberinto
+        System.out.print("|");
+        for (int j=0; j < vec[i].length; j++) {
+          System.out.print (vec[i][j]);
+          if (j!=vec[i].length-1){
+              System.out.print("  ");
+          }
+        }
+        System.out.println("|");
+        }
+    }
+    
+    public void laberintoCompleto(){
+        setupLaberinto(lab1);
+        impresionLab(lab1);
     }
     
 }
