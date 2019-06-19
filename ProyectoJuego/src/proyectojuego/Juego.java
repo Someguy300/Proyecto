6b05c484d328ceac0ad7ffdb04d3aaa1ae947715
Someguy300 <Someguy300@users.noreply.github.com>
2019-06-19 17:28:47 -0400

    Update 0.2 Laberinto
    Agregue las entradas y salidas (asegurandome que no queden en esquinas)

--- ProyectoJuego/src/proyectojuego/Juego.java
+++ ProyectoJuego/src/proyectojuego/Juego.java
@@ -10,26 +10,56 @@ package proyectojuego;
  * @author Jesus Barrios
  */
 public class Juego {
+    //Determinantes paredes del laberinto
     char p = '▓';
     char ent = 'E';
     char sal = 'S';
+    int filaEntrada;
+    int columnaEntrada; 
+    int filaSalida; 
+    int columnaSalida; 
+    
     
     char [][]lab1 = new char[10][10];
     
-    public char [][] setupLaberinto(char [][] vec){
-        for (int i = 0; i < lab1.length; i++) {
-            for (int j = 0; j < lab1[i].length; j++) {
-                if (i==0 || i==9){
-                    lab1[i][j]= p;
-                } else if(j==0 || j==9){
-                    lab1[i][j] = p;
+    public char [][] setupParedesLaberinto(char [][] vec){
+        for (int i = 0; i < vec.length; i++) {
+            for (int j = 0; j < vec[i].length; j++) {
+                if ((i==0 || i==9) || (j==0 || j==9)){ //Determina paredes exteriores
+                    vec[i][j]= p;
                 } else {
-                    lab1[i][j] = '░';
-                }
-                
-                
+                    vec[i][j] = '░';   
+                }   
+            }  
+        }            
+        filaEntrada = (int) ((Math.random()*10));
+        if (filaEntrada == 0 || filaEntrada == 9){
+            columnaEntrada = 1+(int) (Math.random()*8);   
+        }else if (filaEntrada>0 && filaEntrada<9){
+            columnaEntrada = (int) (Math.random()*10);
+            if (columnaEntrada >= 5){
+                columnaEntrada = 9;
+            } else {
+                columnaEntrada = 0;
             }  
         }
+        
+        filaSalida = (int) ((Math.random()*10));
+        if (filaSalida == 0 || filaSalida == 9){
+            columnaSalida = 1+(int) (Math.random()*8);
+        } else if (filaSalida>0 && filaSalida <9){
+            columnaSalida = (int) (Math.random()*10);
+            if (columnaSalida >= 5){
+                columnaSalida = 9;
+            } else {
+                columnaSalida = 0;
+            }  
+        }
+        
+        vec[filaEntrada] [columnaEntrada] = ent;
+        vec[filaSalida] [columnaSalida] = sal;
+        
+        
         return vec;
     }
     
@@ -47,9 +77,37 @@ public class Juego {
         }
     }
     
-    public void laberintoCompleto(){
-        setupLaberinto(lab1);
+    public void laberintoCompleto(){ //Mentira que es el laberinto completo pero mientras, uso esto para ver que imprimo
+        setupParedesLaberinto(lab1);
         impresionLab(lab1);
     }
+    
+    
+    
+    
+    
+    
+    
+    //Setters y Getters
+
+    public int getColumnaEntrada() {
+        return columnaEntrada;
+    }
+
+    public void setColumnaEntrada(int columnaEntrada) {
+        this.columnaEntrada = columnaEntrada;
+    }
+
+    public int getColumnaSalida() {
+        return columnaSalida;
+    }
+
+    public void setColumnaSalida(int columnaSalida) {
+        this.columnaSalida = columnaSalida;
+    }
+    
+    
+    
+    
     
 }
