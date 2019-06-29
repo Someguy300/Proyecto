e97837bec3f5607ad0f303c0b34c5186c710d70e
ricardoiso <51860030+ricardoiso@users.noreply.github.com>
2019-06-29 19:05:24 -0400

    Update Objeto.java

--- ProyectoJuego/src/proyectojuego/Objeto.java
+++ ProyectoJuego/src/proyectojuego/Objeto.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectojuego;
 
 /**
+ * Clase 
  *
  * @author Jesus Barrios
+ * @author Ricardo Serrano
+ * @version
  */
 public class Objeto {
     
+    private int[] pocAditiva= {1, 2, 5, 10, 20};
+    private double[] pocMultiplicativa = {1.10, 1.20, 1.25, 1.5, 2};
+    
+    private Objeto[] bolso = new Objeto[10];
+
+    
+    
+    
+    
+    public double generarPocima(){
+        int tipo = (int) (Math.random()*10); 
+        
+        int aleatorio = (int)(Math.random()*4);
+        
+        if(tipo<5){
+            return pocAditiva[aleatorio];
+        } else {
+            return pocMultiplicativa[aleatorio];
+        }
+    }
+    
+    
+    
+    
+//    public Objeto[] bolsoInicial(Objeto[] bolso){
+//        for (int i = 0; i < 3; i++) {
+//            bolso[i]=generarPocima();
+//        }
+//        return bolso;
+//    }
+    
+    
+    
+    
+    
+    
+    
+//    private pico;
+//    private cuerda; //Este objeto es bonus: debe mostrar salida del laberinto
+
+//    public int getPocAditiva() {
+//        return pocAditiva[(int) (Math.random() * 4)];
+//    }
+//
+//    public double getPocMultiplicativa() {
+//        return pocMultiplicativa[(int) (Math.random() * 4)];
+//    }
+
+
+
+
 }
