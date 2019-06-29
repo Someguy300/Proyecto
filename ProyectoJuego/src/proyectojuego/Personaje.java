5b715dc36ddbbc3f4ae6ca0d3d6b77a31b7ec047
ricardoiso <51860030+ricardoiso@users.noreply.github.com>
2019-06-29 19:06:17 -0400

    Update Personaje.java

--- ProyectoJuego/src/proyectojuego/Personaje.java
+++ ProyectoJuego/src/proyectojuego/Personaje.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectojuego;
 
 /**
+ * Clase que desarrolla el Jugador (personaje principal) del juego
  *
  * @author Jesus Barrios
+ * @author Ricardo Serrano
+ * @version
  */
 public class Personaje {
     
+    protected int vida, fuerza;
+    
+    /**
+     * Constructor del Personaje
+     *
+     * @param vida
+     * @param fuerza
+     */
+    public Personaje(int vida, int fuerza){
+        this.vida= vida;
+        this.fuerza=fuerza;
+    }
+    
+    /**
+     * Constructor del Personaje
+     * 
+     * @param fuerza 
+     */
+    public Personaje(int fuerza){
+        this.fuerza=fuerza;
+    }
 }
