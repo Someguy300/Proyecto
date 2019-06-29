53610e2fb6d9a1fdc7cb9a9d957d4ff062d2b943
ricardoiso <51860030+ricardoiso@users.noreply.github.com>
2019-06-29 18:57:44 -0400

    Update Jugador.java

--- ProyectoJuego/src/proyectojuego/Jugador.java
+++ ProyectoJuego/src/proyectojuego/Jugador.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectojuego;
 
 /**
+ * Clase que desarrolla el Jugador (personaje principal) del juego
  *
  * @author Jesus Barrios
+ * @author Ricardo Serrano
+ * @version
  */
-public class Jugador {
+public class Jugador extends Personaje{
     
+    
+    /**
+     * Constructor del Jugador
+     * 
+     * @param vida
+     * @param fuerza
+     * @param bolso 
+     */
+    public Jugador(int vida, int fuerza, Objeto[] bolso) {
+        super(vida, fuerza);
+        this.vida = 5;
+        this.fuerza = 10;
+        this.bolso=bolso;
+    }
+    
+    private Objeto[] bolso = new Objeto[10];
+    
+    public Objeto[] bolsoInicial(Objeto[] bolso, int i){
+        if (bolso[i] == null){
+        bolso[1] = new Objeto();
+        } else {
+            
+        }
+//        for (int i = 0; i < 3; i++) {
+//            
+//            bolso[i]= ;
+//        }
+    }
+//    
 }
