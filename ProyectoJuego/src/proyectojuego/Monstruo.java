9fb2cdda1b8a68fc903078ad09142febb34f5ebd
ricardoiso <51860030+ricardoiso@users.noreply.github.com>
2019-06-29 19:04:30 -0400

    Update Monstruo.java

--- ProyectoJuego/src/proyectojuego/Monstruo.java
+++ ProyectoJuego/src/proyectojuego/Monstruo.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectojuego;
 
 /**
+ * Clase que desarrolla los Monstruos del juego
  *
  * @author Jesus Barrios
+ * @author Ricardo Serrano
+ * @version
  */
-public class Monstruo {
-    int lol;
+public class Monstruo extends Personaje{
+    
+    
+    public int limSup(int nivelLab){
+        int limSup=nivelLab*nivelLab;
+        return limSup;
+    }
+    
+    public int limInf(int nivelLab){
+        int limInf=(nivelLab*nivelLab)/2;
+        return limInf;
+    }
+//    int limSup = nivelLab*nivelLab;
+//    int limInf= (nivelLab*nivelLab)/2;
+    
+    private Juego j = new Juego();
+    
+    /**
+     * Constructor del Monstruo
+     * 
+     * @param fuerza 
+     */
+    public Monstruo(int fuerza){
+        super(fuerza);
+        this.fuerza= (int)((limSup(j.getNivelLab())-limInf(j.getNivelLab()))*Math.random());          
+        
+    }
 }
