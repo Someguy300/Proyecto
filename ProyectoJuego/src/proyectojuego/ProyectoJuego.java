79a7718a8a1d26304060bdd879ef37f062a63d85
ricardoiso <51860030+ricardoiso@users.noreply.github.com>
2019-06-29 19:06:59 -0400

    Update ProyectoJuego.java

--- ProyectoJuego/src/proyectojuego/ProyectoJuego.java
+++ ProyectoJuego/src/proyectojuego/ProyectoJuego.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectojuego;
 
 /**
+ * Clase que realizará todo el juego
  *
- * @author Jesus Barrios y Ricardo Serrano
+ * @author Jesus Barrios
+ * @author Ricardo Serrano
+ * @version
  */
 public class ProyectoJuego {
 
@@ -15,12 +14,8 @@ public class ProyectoJuego {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Juego j = new Juego(); //Juego seria nuestra clase control
-        Laberinto l = new Laberinto();
-        //j.iniciar;
-        l.laberintoCompleto();
-        
+        Juego j = new Juego();
+        j.iniciar();
     }
     
 }
